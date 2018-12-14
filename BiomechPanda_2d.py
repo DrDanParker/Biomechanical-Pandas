414a878f77c6f66d39571930bc629f65764a081d
Dan Parker <32143583+DrDanParker@users.noreply.github.com>
2018-12-14 21:08:06 +0000

    Create BiomechPanda_2d.py

---
+++ BiomechPanda_2d.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""############################################################################
+###
+### Biomechanical Pandas 2D Time Series
+### This file is part of Biomechanical-Pandas
+### This file was created by Dr Daniel Parker on 14/12/18 
+###    Twitter: @DrDanParker     GitHub:https://github.com/DrDanParker 
+###     
+### Copyright (C) 2018 University of Salford - All Rights Reserved
+### You may use, distribute and modify this code under the terms of MIT Licence
+### See LICENSE or go to https://tldrlegal.com/license/mit-license for full licence details
+###
+### Based on/Learnt from the following:
+###     General panda docs and online tutorials
+###     Learning the Pandas Library - Matt Harrison 2016 
+###
+############################################################################"""
+
