b60162e2181ea1df696b53a925e9073439f360ff
Dan Parker <32143583+DrDanParker@users.noreply.github.com>
2018-12-15 00:30:13 +0000

    Update
    Addition of Descriptive Stats

--- BiomechPanda_2d.py
+++ BiomechPanda_2d.py
@@ -16,3 +16,40 @@
 ###
 ############################################################################"""
 
+import pandas as pd
+
+class Biomech2D:
+    
+    def __init__(self, fname):
+        self.fname = fname
+        self.data = pd.read_csv(self.fname,index_col=0)
+            #other file handling methods @ github.com/DrDanParker/Data-Handling
+
+    def descriptive(self): # pandas does also have a builtin describe function.
+        '''
+        Builds a DataFrame with relevant descriptive statistics
+        ''' 
+        Descriptives = pd.DataFrame(columns=self.data.columns)
+        Descriptives.loc['Count'] = self.data.count()
+        Descriptives.loc['Mean'] = self.data.mean() 
+        Descriptives.loc['SD'] = self.data.std()
+        Descriptives.loc['Variance'] = self.data.var()
+        Descriptives.loc['Sum'] = self.data.sum()
+        Descriptives.loc['Min'] = self.data.min()
+        Descriptives.loc['Max'] = self.data.max()
+        Descriptives.loc['Range'] = Descriptives.loc['Max'] - Descriptives.loc['Min']
+        Descriptives.loc['SEM'] = self.data.sem() # Standard Error of the Mean
+        Descriptives.loc['Skewness'] = self.data.skew()
+        Descriptives.loc['Kurtosis'] = self.data.kurtosis()
+        return(Descriptives)
+        
+        
+
+################################################################################
+###     Run Script
+################################################################################
+
+# fname = '.\example.csv' # either move to working directory or update to location
+fname = 'C:\Syncplicity\GitRepos\Biomechanical-Pandas\example.csv'
+d = Biomech2D(fname)
+d.descriptive()
